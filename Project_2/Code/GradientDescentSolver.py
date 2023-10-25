e3b4fba3df03db834d2805384cc850b2f2588375
Ole kristian Rustebakke <olekristianrustebakke@outlook.com>
2023-10-25 10:27:49 +0200

    GD class update

--- Project_2/Code/GradientDescentSolver.py
+++ Project_2/Code/GradientDescentSolver.py
@@ -1,14 +1,14 @@
 import numpy as np
 from typing import Callable
 import jax.numpy as jnp
-from jax import grad, jit, vmap
-
+from jax import grad, jit, vmap, random
+key = random.PRNGKey(456)
 class Tuning_algorithm:
     def __init__(self):
         self.rho = 0.99
         self.beta1=0.9
         self.beta2=0.999
-        self.delta=1e-8
+        self.delta=1e-7
         self.eta=0.01
         self.Giter=0.0
         self.first_moment = 0.0
@@ -39,31 +39,35 @@ class Tuning_algorithm:
 
 
         elif self.algorithm == 1: #Adagrad
-            Giter = (self.rho*self.Giter+(1-self.rho)*gradients*gradients)
+            self.Giter = self.Giter+gradients*gradients
             update = gradients*self.eta/(self.delta+jnp.sqrt(self.Giter))
 
         elif self.algorithm==2: #RMS-prop
-            self.Giter = (self.rho*self.Giter+(1-rho)*gradients*gradients)
+            self.Giter = (self.rho*self.Giter+(1-self.rho)*gradients*gradients)
             update = gradients*self.eta/(self.delta+jnp.sqrt(self.Giter))
 
         elif self.algorithm == 3: #Adam
             if iter == None:
                 print("Remember to set iter!")
             self.first_moment = self.beta1*self.first_moment + (1-self.beta1)*gradients
-            self.second_moment = self.beta2*self.second_moment+(1-self.beta2)*gradients*gradients
+            self.second_moment = self.beta2*self.second_moment + (1-self.beta2)*gradients*gradients
             first_term = self.first_moment/(1.0-self.beta1**iter)
             second_term = self.second_moment/(1.0-self.beta2**iter)
-            update = self.eta*self.first_term/(jnp.sqrt(self.second_term)+self.delta)
+            update = self.eta*first_term/(jnp.sqrt(second_term)+self.delta)
         return update
 
 
 class Gradient_Descent:
-    def __init__(self, X, y, cost_func, beta):
+    def __init__(self, X, y, cost_func, deg, gradient_func=None):
         self.X = X
         self.y = y
         self.cost_func = cost_func
-        self.beta = beta
-        self.gradient_func = None
+        self.deg = deg
+        self.beta = None
+        if gradient_func == None:
+            self.gradient_func = grad(cost_func)
+        else:
+            self.gradient_func = gradient_func
 
         self.tuning_algorithm = Tuning_algorithm() # Index of 'active' tuning algorithm (chosen from tuning_algorithms)
 
@@ -72,71 +76,98 @@ class Gradient_Descent:
         methods."""
         self.gradient_func = gradient_func
 
+    def set_tuning_parameters(self,rho=0.99,beta1=0.9,beta2=0.999,delta = 1e-8,eta=0.01,Giter=0.0,first_moment=0.0,second_moment=0.0):
+        self.tuning_algorithm.rho = rho
+        self.tuning_algorithm.beta1=beta1
+        self.tuning_algorithm.beta2=beta2
+        self.tuning_algorithm.delta=delta
+        self.tuning_algorithm.eta=eta
+        self.tuning_algorithm.Giter=Giter
+        self.tuning_algorithm.first_moment = first_moment
+        self.tuning_algorithm.second_moment = second_moment
+        return 0
+
 
     def learning_schedule(self, t0, t1, t):
         """ Compute learning rate (might want to have t0 and t1 as class
         attributes instead) """
         return t0/(t + t1)
 
-    def GD(self, n_iter, momentum = None, use_jax = True, algorithm: str="DEFAULT"):
+    def GD(self, n_iter, momentum = None, algorithm: str="DEFAULT"):
         """ Compute gradient descent (with momentum if specified)"""
+        self.beta = random.normal(key,shape=(self.deg+1,1))
+        """
         if use_jax == True:
             gradient_func = grad(self.cost_func)
         else:
             gradient_func = self.gradient_func
+            """
         self.tuning_algorithm.set_tuning_algorithm(algorithm)
 
-        gradient = gradient_func(self.beta)
+        gradient = self.gradient_func(self.beta)
         if momentum == None:
-            for iter in range(n_iter):
+            for iter in range(1,n_iter+1):
                 # gradient = (2.0/n)*X.T @ (X @ beta-y)
-                gradient = gradient_func(self.beta)
+                gradient = self.gradient_func(self.beta)
                 update_beta = self.tuning_algorithm.update(gradient, iter)
                 self.beta -= update_beta
 
+
         else:
-            for iter in range(n_iter):
+            for iter in range(1,n_iter+1):
                 mom_term = momentum*jnp.copy(gradient) #adding the momentum term using the previous gradient
-                gradient = gradient_func(beta)
-                update_beta = self.tuning_algorithm.update(gradient, iter)+mom_term
+                gradient = self.gradient_func(self.beta)
+                update_beta = self.tuning_algorithm.update(gradient, iter) + mom_term
                 self.beta -= update_beta
+
 
         self.tuning_algorithm.set_tuning_algorithm("DEFAULT")
         self.tuning_algorithm.reset_params()
         return self.beta
 
-    def SGD(self, n_iter, n_epochs, batch_size, momentum = None, use_jax = True):
+    def SGD(self, n_iter, n_epochs, batch_size, momentum = None,algorithm: str="DEFAULT"):
+        self.beta = random.normal(key,shape=(self.deg+1,1))
         M = batch_size
         n = len(self.y)
         m = int(n/M) #number of minibatches
         """ Compute stochastic gradient descent (with momentum if specified)"""
+        """
         if use_jax == True:
             gradient_func = grad(self.cost_func)
         else:
             gradient_func = self.gradient_func
+
+            """
         self.tuning_algorithm.set_tuning_algorithm(algorithm)
         if momentum == None:
             for epoch in range(n_epochs):
                 self.tuning_algorithm.reset_params()
-                for i in range(m):
-                    random_index = M*jnp.random.randint(m)
-                    xi = X[random_index:random_index+M]
-                    yi = y[random_index:random_index+M]
-                    gradient = (1.0/M)*gradient_func(yi, xi, beta)
+                for iter in range(1,m+1):
+                    #t = epoch*m + iter
+                    #self.tuning_algorithm.eta = self.learning_schedule(5,50,t)
+                    random_index = M*random.randint(key,shape=(1,),minval=0,maxval=m)[0]
+                    xi = self.X[random_index:random_index+M]
+                    yi = self.y[random_index:random_index+M]
+                    gradient = (1.0/M)*self.gradient_func(self.beta,xi,yi)
                     update_beta = self.tuning_algorithm.update(gradient, iter)
                     self.beta -= update_beta
+
+
 
         else:
             for epoch in range(n_epochs):
                 self.tuning_algorithm.reset_params()
-                for i in range(m):
-                    random_index = M*jnp.random.randint(m)
-                    xi = X[random_index:random_index+M]
-                    yi = y[random_index:random_index+M]
-                    gradient = (1.0/M)*gradient_func(yi, xi, beta)
+                for iter in range(1,m+1):
+                    #t = epoch*m + iter
+                    #self.tuning_algorithm.eta = self.learning_schedule(5,50,t)
+                    random_index = M*random.randint(key,shape=(1,),minval=0,maxval=m)[0]
+                    xi = self.X[random_index:random_index+M]
+                    yi = self.y[random_index:random_index+M]
+                    gradient = (1.0/M)*self.gradient_func(self.beta,xi,yi)
                     mom_term = momentum*jnp.copy(gradient)
                     update_beta = self.tuning_algorithm.update(gradient, iter) + mom_term
                     self.beta -= update_beta
+
 
         self.tuning_algorithm.set_tuning_algorithm("DEFAULT")
 
--- Project_2/Code/test.py
+++ Project_2/Code/test.py
@@ -3,7 +3,6 @@ from GradientDescentSolver import Gradient_Descent
 from NeuralNetwork import Node, Layer
 
 from random import random, seed
-import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
@@ -28,7 +27,7 @@ X = poly.fit_transform(x) # Find feature matrix
 def CostOLS(beta):
     return jnp.sum((y-X @ beta)**2)
 
-beta = random.normal(key,shape=(deg+1,1))
+#beta = random.normal(key,shape=(deg+1,1))
 
 
 n_iter=1000
@@ -36,8 +35,92 @@ XT_X = X.T @ X
 beta_linreg = jnp.linalg.inv(X.T @ X) @ (X.T @ y)
 print("beta from OLS")
 print(beta_linreg)
-
-gd_class = Gradient_Descent(X,y,CostOLS,beta)
+"""
+gd_class = Gradient_Descent(X,y,CostOLS,deg)
+gd_class.set_tuning_parameters(eta=0.001)
 beta = gd_class.GD(n_iter)
 print("beta from GD")
 print(beta)
+
+gd_class = Gradient_Descent(X,y,CostOLS,deg)
+gd_class.set_tuning_parameters(eta=0.001)
+beta = gd_class.GD(n_iter,momentum=0.001)
+print("beta from GD with momentum")
+print(beta)
+
+gd_class = Gradient_Descent(X,y,CostOLS,deg)
+gd_class.set_tuning_parameters(eta=0.001)
+beta = gd_class.GD(n_iter,algorithm="RMSPROP")
+print("beta from GD, using RMS-prop")
+print(beta)
+
+gd_class = Gradient_Descent(X,y,CostOLS,deg)
+gd_class.set_tuning_parameters(eta=0.001)
+beta = gd_class.GD(n_iter,momentum=0.001,algorithm="RMSPROP")
+print("beta from GD with momentum, using RMS-prop")
+print(beta)
+"""
+def CostOLS2(beta,X,y):
+    return jnp.sum((y - X @ beta)**2)
+"""
+gd_class = Gradient_Descent(X,y,CostOLS2,deg)
+gd_class.set_tuning_parameters(eta=10**(-5))
+beta = gd_class.SGD(n_iter,n_epochs=50, batch_size=5)
+print("beta from SGD")
+print(beta)
+
+gd_class = Gradient_Descent(X,y,CostOLS2,deg)
+gd_class.set_tuning_parameters(eta=0.001)
+beta = gd_class.SGD(n_iter,n_epochs=50, batch_size=5,momentum=0.001)
+print("beta from SGD with momentum")
+print(beta)
+
+gd_class = Gradient_Descent(X,y,CostOLS,deg)
+gd_class.set_tuning_parameters(eta=0.001)
+beta = gd_class.GD(n_iter,algorithm="ADAGRAD")
+print("beta from GD, using Adagrad")
+print(beta)
+
+gd_class = Gradient_Descent(X,y,CostOLS,deg)
+gd_class.set_tuning_parameters(eta=0.001)
+beta = gd_class.GD(n_iter,momentum=0.001,algorithm="ADAGRAD")
+print("beta from GD with momentum, using Adagrad")
+print(beta)
+"""
+gd_class = Gradient_Descent(X,y,CostOLS,deg)
+gd_class.set_tuning_parameters(eta=0.001)
+beta = gd_class.GD(n_iter,algorithm="ADAM")
+print("beta from GD, using Adam")
+print(beta)
+
+gd_class = Gradient_Descent(X,y,CostOLS,deg)
+gd_class.set_tuning_parameters(eta=0.001)
+beta = gd_class.GD(n_iter,momentum=0.001,algorithm="ADAM")
+print("beta from GD with momentum, using Adam")
+print(beta)
+
+
+"""
+This is printed:
+beta from OLS
+[[2.0863998]
+ [5.227591 ]
+ [1.7393135]]
+beta from GD
+[[2.086448 ]
+ [5.227451 ]
+ [1.7393789]]
+beta from GD with momentum
+[[2.0863922]
+ [5.2276073]
+ [1.739303 ]]
+beta from GD, using RMS-prop
+[[0.47813433]
+ [3.9825766 ]
+ [0.03088689]]
+beta from GD with momentum, using RMS-prop
+[[2.0864348]
+ [5.2274885]
+ [1.7393607]]
+
+"""
