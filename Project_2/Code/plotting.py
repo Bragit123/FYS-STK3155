a3c725778c5ac3c42f99a24c93363b39672c247f
Bragit123 <b.a.trefjord@fys.uio.no>
2023-11-09 13:25:49 +0100

    Added plotting.py

---
+++ Project_2/Code/plotting.py
@@ -0,0 +1,51 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+label_size = 20
+
+## Regular plotting
+def plot(x_list, y_list, labels, title, xlabel, ylabel, filename):
+    # Turn x, y and labels into lists
+    if type(x_list) == np.ndarray:
+        x_list = [x_list]
+    if type(y_list) == np.ndarray:
+        y_list = [y_list]
+    if type(labels) == str:
+        labels = [labels]
+    
+    plt.figure()
+    plt.title(title, fontsize=label_size)
+    plt.xlabel(xlabel, fontsize=label_size)
+    plt.ylabel(ylabel, fontsize=label_size)
+    plt.yticks(fontsize=label_size)
+    plt.xticks(fontsize=label_size)
+
+    for x, y, label in zip(x_list, y_list, labels):
+        plt.plot(x, y, label=label)
+    
+    plt.legend(fontsize=label_size)
+    plt.savefig(filename, bbox_inches='tight')
+
+## Heatmap
+def heatmap(data, xticks, yticks, title, xlabel, ylabel, filename):
+    fig, ax = plt.subplots(figsize = (10, 10))
+    sns.heatmap(data, xticklabels=xticks, yticklabels=yticks, annot=True, ax=ax, cmap="viridis")
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.yticks(fontsize=label_size)
+    ax.xticks(fontsize=label_size)
+    plt.savefig(filename, bbox_inches='tight')
+
+## Søyleplot
+def barplot(x, y, xlabel, ylabel, title, filename):
+    plt.figure()
+    plt.title(title, fontsize=label_size)
+    plt.xlabel(xlabel, fontsize=label_size)
+    plt.ylabel(ylabel, fontsize=label_size)
+    plt.yticks(fontsize=label_size)
+    plt.xticks(fontsize=label_size)
+    plt.bar(x, y)
+    plt.savefig(filename, bbox_inches='tight')
