99f557bd9cab64aba6911f21da49042987fd1c00
Bragit123 <b.a.trefjord@fys.uio.no>
2023-11-13 10:15:53 +0100

    Updated breast cancer and added plots

--- Project_2/Code/test.py
+++ Project_2/Code/test.py
@@ -7,6 +7,7 @@ from NN import FFNN
 from sklearn import datasets
 from sklearn.preprocessing import minmax_scale
 from sklearn.model_selection import train_test_split
+import plotting
 
 # Load cancer dataset
 cancer = datasets.load_breast_cancer()
@@ -24,7 +25,7 @@ dim = (10, 100, 1)
 hidden_act = sigmoid
 output_act = sigmoid
 cost_func = CostLogReg
-seed = 500
+seed = 100
 classification = True
 
 eta = 0.01
@@ -33,7 +34,7 @@ rho2 = 0.999
 momentum = 0.01
 scheduler = AdamMomentum(eta, rho, rho2, momentum)
 batches = 1
-epochs = 100
+epochs = 50
 lam = 0
 
 # Create and train neural network
@@ -41,22 +42,26 @@ network = FFNN(dim, hidden_act, output_act, cost_func, seed, classification)
 output = network.predict(X_train)
 scores = network.train(X_train, t_train, scheduler, batches, epochs, lam, X_val, t_val)
 
-# Plot error
-epoch_arr = np.arange(epochs)
-plt.figure()
-plt.title("Training errors")
-plt.xlabel("Epoch")
-plt.ylabel("Value")
-plt.plot(epoch_arr, scores["train_errors"], label="Train error")
-plt.plot(epoch_arr, scores["val_errors"], label="Validation error")
-plt.legend()
-plt.savefig("error.pdf")
 
-plt.figure()
-plt.title("Training errors")
-plt.xlabel("Epoch")
-plt.ylabel("Value")
-plt.plot(epoch_arr, scores["train_accs"], label="Train accuracy")
-plt.plot(epoch_arr, scores["val_accs"], label="Validation accuracy")
-plt.legend()
-plt.savefig("accuracy.pdf")
+# Plot error and accuracy
+epoch_arr = np.arange(epochs)
+
+x_plot = [epoch_arr, epoch_arr]
+y_plot = [scores["train_errors"], scores["val_errors"]]
+labels = ["Train error", "Validation error"]
+title = "Error of neural network model on breast cancer dataset."
+xlabel = "Epoch"; ylabel = "Value"
+filename = "error.pdf"
+
+plotting.plot(x_plot, y_plot, labels, title, xlabel, ylabel, filename)
+
+labels = ["Train accuracy", "Validation accuracy"]
+title = "Accuracy of neural network model on breast cancer dataset."
+y_plot = [scores["train_accs"], scores["val_accs"]]
+filename = "accuracy.pdf"
+plotting.plot(x_plot, y_plot, labels, title, xlabel, ylabel, filename)
+
+print(scores["train_errors"][-1])
+print(scores["val_errors"][-1])
+print(scores["train_accs"][-1])
+print(scores["val_accs"][-1])
