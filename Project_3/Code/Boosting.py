7b5eadc621c5a83a3618b6c970ea1be1692e5bb3
Bragit123 <b.a.trefjord@fys.uio.no>
2023-12-15 16:57:09 +0100

    Finished Boosting.py

--- Project_3/Code/Boosting.py
+++ Project_3/Code/Boosting.py
@@ -8,7 +8,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import BaggingClassifier
 from sklearn.ensemble import AdaBoostClassifier
 import sklearn.metrics
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import minmax_scale
 from sklearn.ensemble import GradientBoostingRegressor, GradientBoostingClassifier
 import xgboost as xgb
 from sklearn.metrics import mean_squared_error
@@ -16,172 +16,62 @@ from sklearn.tree import export_graphviz
 from pydot import graph_from_dot_data
 import pandas as pd
 import os
+import plotting
 
 import scikitplot as skplt
 
 from tensorflow.keras import datasets
 
 
-class Boosting: #mangler å brukte y_pred på test dataenls
+class Boosting:
 
-    def __init__(self, X_train, y_train, X_test, y_test, max_depth: int,  n_estimators: int,learning_rate: float, algorthim: str = "SAMME.R" ) -> None:
-
-        self.max_depth = max_depth
+    def __init__(self, X_train, y_train, X_test, y_test) -> None:
         self.X_train = X_train
         self.y_train = y_train
         self.X_test = X_test
         self.y_test = y_test
-        self.n_estimators = n_estimators
-        self.learning_rate = learning_rate
-        self.algorthim = algorthim
+        self.eta = 0.1
+        self.lam = 0.1
 
-        self.scaler = StandardScaler()
-        self.X_train_scaled = self.scaler.fit_transform(self.X_train)
-        self.X_test_scaled = self.scaler.transform(self.X_test)
-
-
- #AdaBoost (Adaptive Boosting), Gradient Boosting, and XGBoost.
-
+    # AdaBoost (Adaptive Boosting), Gradient Boosting, and XGBoost.
     def AdaBoost(self):
-
-        ada_clf = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1), n_estimators=self.n_estimators,
-                                      algorithm=self.algorthim, learning_rate=self.learning_rate, random_state=42)
-
+        ada_clf = AdaBoostClassifier(learning_rate=self.eta, random_state=42)
         
-
         #dont scale y_data
-        ada_clf.fit(self.X_train_scaled, self.y_train)
+        ada_clf.fit(self.X_train, self.y_train)
+        y_pred = ada_clf.predict(self.X_test)
+        accuracy = np.mean(y_pred == self.y_test)
 
-        y_pred = ada_clf.predict(self.X_test_scaled)
+        return accuracy
+    
+
+    def Gradient_Boosting(self):
+        model = GradientBoostingClassifier(learning_rate=self.eta)
+
+        model.fit(self.X_train, self.y_train)
+        y_pred = model.predict(self.X_test)
+
+        accuracy = np.mean(y_pred == self.y_test)
+        
+        return accuracy
+    
+
+    def XGBoost(self):
+        model =  xgb.XGBClassifier(learning_rate = self.eta, reg_lambda = self.lam)
+
+        model.fit(self.X_train, self.y_train)
+        y_pred = model.predict(self.X_test)
 
         accuracy = np.mean(y_pred == self.y_test)
 
-        y_probas = ada_clf.predict_proba(self.X_test)
-
-
-        #print(f"Accuracy:{np.mean(y_pred == self.y_test)}")
-        #print(f"self.y_test. {len(self.y_test)}")
-        #print(f"{len(y_pred)}")
-
-        #print(y_probas)
-
-        return y_pred,y_probas, accuracy
-    
-
-    def Gradient_Boosting(self): #change to classification
-
-        #model = GradientBoostingRegressor(max_depth=self.max_depth, n_estimators=self.n_estimators, learning_rate= self.learning_rate)
-
-        model = GradientBoostingClassifier(max_depth = self.max_depth, n_estimators=self.n_estimators, learning_rate=self.learning_rate)  
-
-        model.fit(self.X_train_scaled,self.y_train)
-        y_pred = model.predict(self.X_test_scaled)
-
-        accuracy = np.mean(y_pred == self.y_test)
-
-        print(f"y_pred: {y_pred}")
-
-        #print(f"accuracy:{accuracy} ")
-
-        y_probas = model.predict_proba(self.X_test)
-        
-        return y_pred,y_probas, accuracy
-    
-
-    def XGBoost(self,maxdegree):
-
-        error = np.zeros(maxdegree)
-        bias = np.zeros(maxdegree)
-        variance = np.zeros(maxdegree)
-        polydegree = np.zeros(maxdegree)
-        predictions = list()
-        accuracy = np.zeros(maxdegree)
-
-        for degree in range(maxdegree):
-            
-            #model =  xgb.XGBRegressor(objective ='reg:squarederror', colsaobjective ='reg:squarederror', colsample_bytree = 0.3, learning_rate = 0.1,max_depth = self.max_depth, alpha = 10, n_estimators = 200)
-            model =  xgb.XGBClassifier(objective ='reg:squarederror', colsample_bytree = 0.3, learning_rate = 0.1,
-                                       max_depth = self.max_depth, alpha = 10, n_estimators = 200)
-
-            #should max depth be equal to degree ? 
-
-            model.fit(self.X_train_scaled,self.y_train)
-
-            #get no changes in accuracy for higher degrees 
-
-            y_pred = model.predict(self.X_test_scaled)
-            #print(y_pred)
-
-            y_probas = model.predict_proba(self.X_test)
-
-            predictions.append(y_pred)
-
-            accuracy[degree] = np.mean(y_pred == self.y_test)
-
-            polydegree[degree] = degree
-
-            error[degree] = np.mean(np.mean((self.y_test - y_pred)**2))
-
-            bias[degree] = np.mean((self.y_test - np.mean(y_pred))**2)
-
-            variance[degree] = np.mean(np.var(y_pred))
-
-            print('Max depth:', degree)
-
-            print('Error:', error[degree])
-            print('Bias^2:', bias[degree])
-            print('Var:', variance[degree])
-        
-            print('{} >= {} + {} = {}'.format(error[degree], bias[degree], variance[degree],  bias[degree] + variance[degree]))
-
-        print(accuracy)
-        print(f"Higest accuray:{np.max(accuracy)} for Degree:{np.argmax(accuracy)}")
-
-        return predictions, y_probas
-    
-    def cumulative_gain(self):
-
-        #y_probas = self.Gradient_Boosting()[0]
-        y_probas = self.AdaBoost()[1]
-        skplt.metrics.plot_cumulative_gain(self.y_test, y_probas, title='Cumulative Gains Curve AdaBoost')
-        y_probas = self.Gradient_Boosting()[1]
-        skplt.metrics.plot_cumulative_gain(self.y_test, y_probas, title='Cumulative Gains Curve Gradient Boost')
-        y_probas = self.XGBoost(6)[1]
-        skplt.metrics.plot_cumulative_gain(self.y_test, y_probas, title='Cumulative Gains Curve XGBoost')
-        plt.show()
-
-    def ROC(self):
-
-        skplt.metrics.plot_roc(self.y_test, self.AdaBoost()[1], title='ROC Curves AdaBoost')
-        skplt.metrics.plot_roc(self.y_test, self.Gradient_Boosting()[1], title='ROC Curves Gradient Boost')
-        skplt.metrics.plot_roc(self.y_test, self.XGBoost(6)[1], title='ROC Curves XGBoost')
-
-        plt.show()
-
-
-    def Descision_Tree(self):
-
-        # dataframe = pd.DataFrame(self.data.data, columns=self.data.feature_names)
-
-        # print(dataframe)
-
-        # y = pd.Categorical.from_codes(self.data.target, self.data.target_names)
-        # y = pd.get_dummies(y)
-
-        # print(y)
-
-        # X_train, X_test, y_train, y_test = train_test_split(dataframe, y,
-        # random_state=1)
-        X_train, X_test, y_train, y_test = self.X_train, self.X_test, self.y_train, self.y_test
-
-        tree_model = DecisionTreeClassifier(max_depth=self.max_depth)
-        tree_model.fit(X_train, y_train)
-
-
-                        
-
+        return accuracy
 
 (X_train, y_train), (X_test, y_test) = datasets.mnist.load_data()
+
+n_train = int(0.1*len(y_train))
+n_test = int(0.1*len(y_test))
+X_train = X_train[0:n_train,:,:] ; y_train = y_train[0:n_train]
+X_test = X_test[0:n_test,:,:] ; y_test = y_test[0:n_test]
 
 n_train, n_rows, n_cols = np.shape(X_train)
 n_test, n_rows, n_cols = np.shape(X_test)
@@ -189,29 +79,40 @@ n_features = n_rows*n_cols
 X_train = np.reshape(X_train, (n_train, n_features))
 X_test = np.reshape(X_test, (n_test, n_features))
 
-instance = Boosting(X_train, y_train, X_test, y_test, 3, 100, 1)
+X_train = minmax_scale(X_train, feature_range=(0, 1), axis=0)
+X_test = minmax_scale(X_test, feature_range=(0, 1), axis=0)
 
+# Make arrays for eta and lambda
+eta0 = -5; eta1 = 1; n_eta = eta1-eta0+1
+lam0 = -5; lam1 = 1; n_lam = lam1-lam0+1
+etas = np.logspace(eta0, eta1, n_eta)
+lams = np.logspace(lam0, lam1, n_lam)
 
-print(instance.AdaBoost()[1])
-print(instance.cumulative_gain())
-print(instance.XGBoost(6))
-instance.ROC()
-instance.Descision_Tree()
+val_accs_adaboost = np.zeros(n_eta)
+val_accs_gradboost = np.zeros(n_eta)
+val_accs_xgboost = np.zeros((n_eta, n_lam))
 
+instance = Boosting(X_train, y_train, X_test, y_test)
 
+for i in range(len(etas)):
+    print(f"i = {i+1} / {n_eta}")
+    instance.eta = etas[i]
+    val_accs_adaboost[i] = instance.AdaBoost()
+    val_accs_gradboost[i] = instance.Gradient_Boosting()
+    
+    for j in range(len(lams)):
+        print(f"j = {j+1} / {n_lam}")
 
-"""
+        instance.lam = lams[j]
+        val_accs_xgboost[i,j] = instance.XGBoost()
 
-skplt.metrics.plot_confusion_matrix(y_test, y_pred, normalize=True)
-
-plt.show()
-y_probas = ada_clf.predict_proba(X_test)
-
-c
-plt.show()
-
-skplt.metrics.plot_cumulative_gain(y_test, y_probas)
-plt.show()
-
-
-"""
+# Create heatmap plots for the accuracies
+title_adaboost = f"Validation accuracies for Adaboost."
+title_gradboost = f"Validation accuracies for Gradient Boost."
+title_xgboost = f"Validation accuracies for XGBoost."
+filename_adaboost = f"../Figures/val_accs_adaboost.pdf"
+filename_gradboost = f"../Figures/val_accs_gradboost.pdf"
+filename_xgboost = f"../Figures/val_accs_xgboost.pdf"
+plotting.barplot(x=np.log10(etas), y=val_accs_adaboost, title=title_adaboost, xlabel="$\\eta$", ylabel="Accuracy", filename=filename_adaboost)
+plotting.barplot(x=np.log10(etas), y=val_accs_gradboost, title=title_gradboost, xlabel="$\\eta$", ylabel="Accuracy", filename=filename_gradboost)
+plotting.heatmap(data=val_accs_xgboost, xticks=lams, yticks=etas, title=title_xgboost, xlabel="$\\lambda$", ylabel="$\\eta$", filename=filename_xgboost)
--- Project_3/Code/plotting.py
+++ Project_3/Code/plotting.py
@@ -54,8 +54,7 @@ def barplot(x, y, xlabel, ylabel, title, filename):
     if type(x[0]) == str:
         plt.xticks(fontsize=label_size, rotation=90)
     plt.bar(x, y, label=x)
-    indices = np.arange(len(x))
     for i in range(len(x)):
-        plt.text(indices[i], y[i], f"{y[i]:.2f}", ha="center", va="bottom")
+        plt.text(x[i], y[i], f"{y[i]:.2f}", ha="center", va="bottom")
     plt.savefig(filename, bbox_inches='tight')
     plt.close()
